1754ae7075ae43c478aa5d4ebdb192108a8b2636
ScriptCamilo <script.camilo@gmail.com>
2022-10-13 12:11:30 -0300

    feat: Add all project in its first commit

---
+++ src/components/index.tsx
@@ -0,0 +1,14 @@
+// Components
+export { default as Text } from './Text/Text';
+export { default as Heading } from './Heading/Heading';
+export { default as Button } from './Button/Button';
+export { TextInput } from './TextInput/TextInput';
+export { default as Checkbox } from './Checkbox/Checkbox';
+
+// Interfaces/Type
+export type { TextProps } from './Text/Text';
+export type { HeadingProps } from './Heading/Heading';
+export type { ButtonProps } from './Button/Button';
+export type { TextInputInputProps } from './TextInput/TextInput';
+export type { TextInputRootProps } from './TextInput/TextInput';
+export type { CheckboxProps } from './Checkbox/Checkbox';
